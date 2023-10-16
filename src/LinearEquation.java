d31a67992b36891fadc21e9216248eeb87dc2df8
BT_1N3_15 <sample@bths.edu>
2023-10-16 09:31:34 -0400

    added a few methods in LinearEquation and LinearEquationLogic

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    private int x1, y1, x2, y2;;
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public double distance() {
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return (double) Math.round(distance * 100) / 100;
+    }
+
+    public double yIntercept() {
+        return 0;
+    }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class LinearEquationLogic {
+import javax.sound.sampled.Line;
+import java.util.Scanner;
+public class LinearEquationLogic {
+    int x1, y1, x2, y2;
+    public LinearEquationLogic() {}
+    public void start() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the Linear Equation Calculator");
+        System.out.print("Enter coordinate pair 1: ");
+        String coord1 = scan.nextLine();
+        System.out.print("Enter coordinate pair 2: ");
+        String coord2 = scan.nextLine();
+
+        x1 = Integer.parseInt(coord1.substring(1, coord1.indexOf(",")));
+        y1 = Integer.parseInt(coord1.substring(coord1.indexOf(",") + 2, coord1.indexOf(")")));
+        x2 = Integer.parseInt(coord1.substring(1, coord2.indexOf(",")));
+        y2 = Integer.parseInt(coord1.substring(coord2.indexOf(",") + 2, coord2.indexOf(")")));
+        LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
+    }
+
 }
