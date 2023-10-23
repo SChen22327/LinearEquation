821347da02fc262aeb6b6a51d0d843f45f4429e4
BT_1N3_15 <sample@bths.edu>
2023-10-23 09:33:32 -0400

    Finished, just need to test

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,12 +1,17 @@
 public class LinearEquation {
     private int x1, y1, x2, y2;
+    private boolean undefined;
     public LinearEquation(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
     }
-
+    public LinearEquation(boolean undefined, int y1, int y2) {
+        this.y1 = y1;
+        this.y2 = y2;
+        this.undefined = undefined;
+    }
     public double roundedToHundredth(double toRound) {
         return (double) Math.round(toRound * 100) / 100;
     }
@@ -15,35 +20,69 @@ public class LinearEquation {
         return roundedToHundredth(distance);
     }
 
+    public double slope() {
+        double slope = y2 - y1;
+        slope /= x2 - x1;
+        return slope;
+    }
+
     public double yIntercept() {
         double yIntercept = y1;
         yIntercept -= slope() * x1;
         return roundedToHundredth(yIntercept);
     }
 
-    public double slope() {
-        double slope = y2 - y1;
-        slope /= x2 - x1;
-        return roundedToHundredth(slope);
-    }
-
     public String equation() {
-        String equation = "y = ";
-        if (slope() == -1) {
-            equation += "-x ";
-        } else if (slope() == 0) {}
-        else if (slope() / (int) slope() != 1) {
-            for (int i = 2; i > 1; i++) {
-                double num = slope();
-                num *= i;
-                if (num / (int) num == 1) {
-                    equation += num + "/" + i;
-                    break;
+        String equation;
+        if (undefined == false) {
+            equation = "y = ";
+            if (slope() == -1) {
+                equation += "-x ";
+            } else if (slope() == 0) {
+                if (yIntercept() == 0) {
+                    equation += yIntercept();
                 }
+            } else if (slope() / (int) slope() != 1) {
+                for (int i = 2; i > 1; i++) {
+                    double num = slope();
+                    num *= i;
+                    if (num / (int) num == 1) {
+                        equation += (int) num + "/" + i;
+                        break;
+                    }
+                }
+                equation += "x ";
+            } else {
+                equation += roundedToHundredth(slope()) + "x ";
+            }
+            if (yIntercept() > 0) {
+                equation += "+ " + yIntercept();
+            } else if (yIntercept() < 0) {
+                equation += "- " + Math.abs(yIntercept());
             }
         } else {
-            equation += "x ";
+            equation = "x = " + x1;
         }
+        return equation;
+    }
 
+    public String coordinateForX(double x) {
+        return "(" + x + ", " + (slope() * x + yIntercept()) + ")";
+    }
+
+    public String lineInfo() {
+        String lineInfo;
+        lineInfo = "The two points are (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")\n";
+        lineInfo += "The equation of the line between these points is: " + equation() + "\n";
+        lineInfo += "The slope of the line is: ";
+        if (undefined) {
+            lineInfo += "undefined\n";
+            lineInfo += "The x-intercept of this line is: " + x1 + "\n";
+        } else {
+            lineInfo += roundedToHundredth(slope()) + "\n";
+            lineInfo += "The y-intercept of this line is: " + yIntercept() + "\n";
+        }
+        lineInfo += "The distance between these points is: " + distance();
+        return lineInfo;
     }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -2,13 +2,21 @@ import javax.sound.sampled.Line;
 import java.util.Scanner;
 public class LinearEquationLogic {
     int x1, y1, x2, y2;
+    double numForX;
+    private boolean endLoop = false;
+    private LinearEquation equation;
     private Scanner scan;
     public LinearEquationLogic() {
         scan = new Scanner(System.in);
     }
     public void start() {
-        getCoords();
-
+        while (endLoop == false) {
+            getCoords();
+            createLineEquation();
+            System.out.println(equation.lineInfo());
+            System.out.println(getCoordForX());
+            endLoop = endLoop();
+        }
     }
 
     public void getCoords() {
@@ -22,8 +30,38 @@ public class LinearEquationLogic {
         y1 = Integer.parseInt(coord1.substring(coord1.indexOf(",") + 2, coord1.indexOf(")")));
         x2 = Integer.parseInt(coord2.substring(1, coord2.indexOf(",")));
         y2 = Integer.parseInt(coord2.substring(coord2.indexOf(",") + 2, coord2.indexOf(")")));
+    }
 
-        LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
-        //System.out.println(x1 + " " + y1 + " " + x2 + " " + y2);
+    public boolean checkUndefined() {
+        try {
+            int testNum = x1 / x2;
+        } catch (Exception e) {
+            return true;
+        }
+        return false;
+    }
+
+    public void createLineEquation() {
+        if (checkUndefined() == false) {
+            equation = new LinearEquation(x1, y1, x2, y2);
+            //System.out.println(x1 + " " + y1 + " " + x2 + " " + y2);
+        } else {
+            equation = new LinearEquation(checkUndefined(), y1, y2);
+        }
+    }
+
+    public String getCoordForX() {
+        System.out.print("Enter an x-value: ");
+        numForX = scan.nextDouble();
+        return equation.coordinateForX(numForX);
+    }
+
+    public boolean endLoop() {
+        System.out.println("Would you like to enter a new pair of coordinates? y/n");
+        if (scan.nextLine().equals("n") || scan.nextLine().equals("no")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
